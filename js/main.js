fa0ad83c92263b775024c24018cd0c1e7904e8c4
giuliatognali <giuliatognali@gmail.com>
2023-02-24 11:03:35 +0100

    js fix

--- js/main.js
+++ js/main.js
@@ -6,35 +6,29 @@ const cell = document.createElement ('div');
 //ciclo for con 4 if differenti 
 for(let i = 1; i <= 100; i++){
 
+    const cell = document.createElement ('div');
+    container.append(cell);
+
     if (i % 15 === 0) {
         console.log('fizzbuzz');
-        const cell = document.createElement ('div');
         cell.classList.add('div-fizzbuzz');
         cell.append('fizzbuzz');
-        container.append(cell);
     }
 
     else if (i % 3 === 0){
         console.log('fizz');
-        const cell = document.createElement ('div');
         cell.classList.add('div-fizz');
         cell.append('fizz');
-        container.append(cell);
-
     }
     else if  (i % 5 === 0){
         console.log('buzz');
-        const cell = document.createElement ('div');
         cell.classList.add('div-buzz');
         cell.append('buzz');
-        container.append(cell);
     }
     else{
         console.log(i);
-        const cell = document.createElement ('div');
         cell.classList.add('div-number');
         cell.append(i);
-        container.append(cell);
     }
     
 }
